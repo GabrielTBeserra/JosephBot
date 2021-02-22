840442b3fbea0c648144d57cf6072aa1ec7a4d62
Gabriel Teles Beserra <gabrieltbeserra@gmail.com>
2021-02-22 10:16:32 -0300

    Corrigido erro da heroku

--- index.py
+++ index.py
@@ -64,13 +64,4 @@ if __name__ == "__main__":
                         f"\nFalha no carregamento do modulo {type(exce).__name__}!\n{exce} ❌")
 
 
-id = ''
-print(os.getenv("TOKEN"))
-print(os.environ.get("TOKEN"))
-
-if os.getenv("TOKEN") != None:
-    id = os.getenv("TOKEN")
-elif os.environ.get("TOKEN") != None:
-    id = os.environ.get("TOKEN")
-
-bot.run(id)
+bot.run(os.getenv("TOKEN"))
